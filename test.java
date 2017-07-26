f259f2a564ade5631e5c3e56eb578d48fa8a0cb4
freedoom-yong <30432749+freedoom-yong@users.noreply.github.com>
2017-07-26 10:33:54 +0800

    创建test.java文件
    这是个简单的九九乘法表

---
+++ test.java
@@ -0,0 +1,14 @@
+
+ class Test{ 
+ public static void main(String[] args){   
+ int n=0,i=0,j=0;   
+ System.out.print("九九乘法表\n");   
+ for(i=1;i<10;i++){   
+ for(j=1;j<i+1;j++)    {   
+ n=i*j;    
+ System.out.print(""+i+"x"+j+"="+n+"\t");  
+ }   
+ System.out.println(""); 
+ }
+ }
+ }
